35c1b185c568a39d1a12e2569a6a6884b5a93cbc
nunppong <kimminki04184@gmail.com>
2020-07-14 09:20:32 +0900

    0714 change

--- 0714/src/TestExam.java
+++ 0714/src/TestExam.java
@@ -3,11 +3,13 @@ public class TestExam {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		for(int i=1; i<=10; i++) {
+		for(int i=1; i<=5; i++) {
 			System.out.print(i + " ");
-			if(i==5) {
-				System.out.println(" ");
-			}
+		}
+		System.out.println(" ");
+		
+		for(int j=10; j>5; j--) {
+			System.out.print(j + " ");
 		}
 	}
 
