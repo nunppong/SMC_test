1af7dbe0e4a28be910b08ad9fb05db1b2f47fcce
nunppong <kimminki04184@gmail.com>
2020-07-14 09:13:48 +0900

    0714_commit

---
+++ 0714/src/TestExam.java
@@ -0,0 +1,14 @@
+
+public class TestExam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1; i<=10; i++) {
+			System.out.print(i + " ");
+			if(i==5) {
+				System.out.println(" ");
+			}
+		}
+	}
+
+}
